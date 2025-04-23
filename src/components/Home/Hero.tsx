955e70b9cc8844dc5649ca447ed8e9909eaf7d8c
Muhammedrameesz <rameezta555@gmail.com>
2025-04-23 12:24:23 +0530

    new

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -7,7 +7,7 @@ import Logo from "@/images/RaP7 Step by Step Procedure_page-0003.jpg";
 
 const HeroBanner: React.FC = () => {
   return (
-    <section className="bg-white w-full h-[500px] flex items-center justify-center overflow-hidden">
+    <section className="bg-white w-full h-[600px] flex items-center justify-center overflow-hidden">
       <div className="w-full h-full relative">
         <Image
           src={Logo}
